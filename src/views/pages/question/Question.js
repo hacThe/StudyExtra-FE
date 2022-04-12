39edc156989a38e1e0d62072956c35bb03bfd9ab
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-04-12 23:56:37 +0700

    bug picture slider

--- src/views/pages/question/Question.js
+++ src/views/pages/question/Question.js
@@ -5,16 +5,51 @@ import './scss/Question.scss';
 import { AiFillLike } from "react-icons/ai";
 import { FaCommentAlt } from "react-icons/fa";
 import CommentItem from './components/CommentItem';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+
+// import { MdArrowForwardIos, MdArrowBackIosNew } from "react-icons/md";
+// const ArrowLeft = (props) => (
+//     <button
+//         {...props}
+//         className='prev w-[60px] h-[60px] bg-white rounded-[100%] border-[1px] border-[#2BBCBA] 
+//         absolute z-10 left-[-30px] top-[30%]'
+//     >
+//         <MdArrowBackIosNew size={26} className='ml-[15px] text-[#2BBCBA]'/>
+//     </button>   
+// );
+// const ArrowRight = (props) => (
+//     <button
+//         {...props}
+//         className='next w-[60px] h-[60px] bg-white rounded-[100%] border-[1px] border-[#2BBCBA] 
+//         absolute z-10 right-[-30px] top-[30%]'
+//     >
+//         <MdArrowForwardIos size={26} className='ml-[17px] text-[#2BBCBA]'/>
+//     </button>
+// );
 
 const Question = () => {
-  return (
+    const slideImages = [
+        {
+            url: 'https://gamek.mediacdn.vn/133514250583805952/2021/9/2/base64-1630595438805599368242.png',
+            caption: 'Image 1'
+        },
+        {
+            url: 'https://gamek.mediacdn.vn/133514250583805952/2021/9/2/base64-1630595438805599368242.png',
+            caption: 'Image 2'
+        },
+        {
+            url: 'https://gamek.mediacdn.vn/133514250583805952/2021/9/2/base64-1630595438805599368242.png',
+            caption: 'Image 3'
+        },
+    ];
+    return (
     <div className="question-page-container">
         <div className="question-container">
             <div className="question-body">
                 <div className="question-header">
                     <img className="user-avatar"
                         src="https://i.pinimg.com/474x/84/e0/08/84e008e416a5662ada45185058678ed7.jpg"
-                        
                     ></img>
                     <p className="user-name">Raiden Ei</p>
                     <p className="post-time">3 phút trước</p>
@@ -23,9 +58,18 @@ const Question = () => {
                     <p className="question-content">
                         Mọi người có thể giúp em so sánh sự khác nhau giữa beautiful và handsome được không ạ, quả là khó khăn đó nha hahaha đạy là câu hỏi ví dụ thoi
                     </p>
-                    <img className="question-picture"
-                        src="https://gamek.mediacdn.vn/133514250583805952/2021/9/2/base64-1630595438805599368242.png"
-                    ></img>
+                    <div className='question-picture-container'>
+                    <Slide>
+                        {slideImages.map((slideImage, index)=> (
+                            <div className="each-slide" key={index}>
+                            <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                                <span>{slideImage.caption}</span>
+                            </div>
+                            </div>
+                        ))} 
+                    </Slide>                                                                        
+                    </div>
+                    
                     <div className="picture-pagination">
                         <div className="tab active"></div>
                         <div className="tab"></div>
