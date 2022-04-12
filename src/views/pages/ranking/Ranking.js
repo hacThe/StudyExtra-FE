c6bc523a2e1e1671a01e47c4a22616fc2ba148a2
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-04-12 20:39:01 +0700

    pagination item in ranking ok

--- src/views/pages/ranking/Ranking.js
+++ src/views/pages/ranking/Ranking.js
@@ -1,15 +1,18 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 
 import './scss/Rankings.scss';
 import UserRankingCard from './components/UserRankingCarrd';
 
-import Pagination from './components/Pagination';
+// import Pagination from './components/Pagination';
 import { Button, ButtonGroup, Grid } from '@mui/material';
 
 import { GiLaurelCrown } from "react-icons/gi";
 import { rankingActions } from '../../../actions/ranking.actions';
 import { useDispatch, useSelector } from "react-redux";
 
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import { IoIosArrowBack, IoIosArrowForward} from "react-icons/io";
 
 const Ranking = () => {
 
@@ -19,6 +22,17 @@ const Ranking = () => {
         setTypeSelect(newValue);
     }
 
+    const [page, setPage] = useState(1);
+    useEffect(() => {
+        setPage(1)
+    }, 1)
+
+    const setCurrentPage = (num) => {
+        setPage(num)
+    }
+
+    var arr = Array.from(Array(95).keys());
+    var maxItem = 10;
     return (
         <div className="ranking-page-container">
             <div className="ranking-container">
@@ -45,25 +59,32 @@ const Ranking = () => {
                         </ButtonGroup>
                     </div>
                     <div className='ranking-list'>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={false}/>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={false}/>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={false}/>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={false}/>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={false}/>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={false}/>
-                        <UserRankingCard isOdd={true}/>
-                        <UserRankingCard isOdd={false}/>
-                        <UserRankingCard isOdd={true}/>
+                        {
+                            arr.length === 0 ? (null) : 
+                                arr.map((val,index) => (
+                                    (index < page * maxItem && index >= (page-1) * maxItem) ? (
+                                        <UserRankingCard index={index+1}/>
+                                    ) : (null)
+                                ))
+                        }
                     </div>
                     <div className='ranking-footer'>
-                        <Pagination page={9} />
+                        <Pagination
+                            count={Math.ceil(arr.length/maxItem)}
+                            renderItem={(item) => (
+                                <PaginationItem
+                                    components={{ 
+                                        previous: IoIosArrowBack, 
+                                        next:  IoIosArrowForward 
+                                    }}
+                                    {...item}
+                                />
+                            )}
+                            onChange = {(e, page) => {
+                                console.log("abc", page);
+                                setCurrentPage(page)
+                            }}
+                        />
                     </div>
                 </div>
             </div>
