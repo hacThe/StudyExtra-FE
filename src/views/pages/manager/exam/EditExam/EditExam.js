9a0bf8250bd3adde649a5d91b60d10a899627a42
D.hacThe <hienthe473@gmail.com>
2022-04-13 18:46:09 +0700

    finish ui ExamSingleForm

--- src/views/pages/manager/exam/EditExam/EditExam.js
+++ src/views/pages/manager/exam/EditExam/EditExam.js
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import SingleExamForm from "../component/SingleExamForm";
+import BackToPageButton from "../../../../components/BackToPageButton";
+import LeadingIconButton from "../../../../components/LeadingIconButton";
+import {
+  AiOutlineExport,
+  AiOutlineEdit,
+  AiOutlineDelete,
+} from "react-icons/ai";
+import { BiHide } from "react-icons/bi";
+import './EditExam.scss'
 function EditExam(props) {
-    return (
-        <div className="manager-fa-ke-modal">
-            <h1>This is edit exam</h1>
+  return (
+    <div className="manager-fa-ke-modal edit-exam-wrapper">
+      <div className="justify-content-between top-action-bar">
+        <BackToPageButton content="Trở lại trang trước" />
+        <div className="course-action align-center">
+          <LeadingIconButton icon={<BiHide />} content="Ẩn" />
+          <LeadingIconButton icon={<AiOutlineDelete />} content="Xóa" />
         </div>
-    );
+      </div>
+      <SingleExamForm/>
+    </div>
+  );
 }
 
 export default EditExam;
