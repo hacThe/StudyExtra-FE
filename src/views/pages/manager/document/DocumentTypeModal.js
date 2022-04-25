a848abc33b4528c9c1bb249cc002e7ad35734670
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-04-25 10:22:40 +0700

    document select UI ok

--- src/views/pages/manager/document/DocumentTypeModal.js
+++ src/views/pages/manager/document/DocumentTypeModal.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import './scss/DocumentTypeModal.scss';
 import { useDispatch, useSelector } from "react-redux";
 import {documentActions} from '../../../../actions/document.actions.js'
+import { MdModeEdit } from "react-icons/md";
+import { AiFillCloseCircle} from "react-icons/ai";
+
 
 function DocumentTypeModal() {
     const dispatch = useDispatch();
@@ -10,8 +13,63 @@ function DocumentTypeModal() {
         dispatch(documentActions.changeModalStatus(false));
     }
 
-    const documentTypes = ["Lớp 12", "Lớp 11", "Lớp 10"];
 
+    const [documentType, setDocumentType] = useState([
+        {
+            type: "Lớp 10",
+            active: false,
+        },
+        {
+            type: "Lớp 11",
+            active: false,
+        },
+        {
+            type: "Lớp 12",
+            active: false,
+        },
+        {
+            type: "Anh văn giao tiếp",
+            active: true,
+        },
+        {
+            type: "Khác",
+            active: false
+        },
+    ]);
+
+    const changeDocument = (documentObject) => {
+        setDocumentType(documentType);
+    }
+    
+    const changeDocumentAt = (index, value) => {
+        var tempt = [];
+        for(var i = 0; i < documentType.length; i++)
+        {
+            if(i!=index)
+            {
+                tempt.push(documentType[i])
+            }
+            else {
+                tempt.push({
+                    type: documentType[i].type,
+                    active : value
+                })
+            }
+        }
+        setDocumentType(tempt);
+        console.log("tempt", tempt);
+    }
+
+
+    console.log(documentType);
+
+    const handleClickType = (e, index) => {
+        console.log("e", e.target.classList.value);
+        if(e.target.classList.value != ''){
+            console.log("Select");
+            changeDocumentAt(index, true)
+        }
+    }
     return (
         <div className="overlay-modal">
             <div 
@@ -24,12 +82,39 @@ function DocumentTypeModal() {
                 </div>
                 <div className="document-type-container">
                     {
-                        documentTypes.map((type)=>{
-                            return (<div>{type}</div>)
+                        documentType.map((type, index)=>{
+                            return (
+                                <div 
+                                    className= {type.active ? 'type-container active' : 'type-container '}
+                                    onClick = {(e) => handleClickType(e, index)}
+                                >
+                                    <div className='type-name'>
+                                        {type.type}
+                                    </div>
+                                    <div className='edit-type'>
+                                        <MdModeEdit size={16} />
+                                    </div>
+                                    <div 
+                                        className='type-delete'
+                                        onClick={()=> changeDocumentAt(index, false)}
+                                    >
+                                        <AiFillCloseCircle size={16} />
+                                    </div>
+                                </div>
+                            )
                         })
                     }
                 </div>
+                <div className="insert-document-type">
+                    <input type='text' className='input-document-type'>
+
+                    </input>
+                    <button className="button-add-type">
+                        Thêm 
+                    </button>
+                </div>
             </div>
+            
         </div>
     );
 }
