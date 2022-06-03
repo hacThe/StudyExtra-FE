24b159d60923ef6a49b9eb17d0ec6ce81164f588
TanThanh-CityBoi <tanthanhe@gmail.com>
2022-06-03 18:13:52 +0700

    fix notification header, code trang result exam

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -267,7 +267,7 @@ function getUserNotifications() {
   return (dispatch) => {
     dispatch(request());
     usersServices.getUserNotifications().then(
-      (userNotifications) => dispatch(success(userNotifications)),
+      (userNotifications) => dispatch(success(userNotifications.data)),
       (error) => dispatch(failure(error.toString()))
     );
   };
