3d4fbfa3ee3f5fc2cf609727ee947ff972da12f3
Dương Hiển Thế <61902982+hacThe@users.noreply.github.com>
2022-06-17 18:52:20 +0700

    Update config.js
    update api in heroku

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -1,5 +1,5 @@
 // thay đổi api way ở đây nek
-const URL_SYSTEM_V1 = "http://localhost:5000/api";
+const URL_SYSTEM_V1 = "https://study-extra-be.herokuapp.com/api";
 export default {
   URL_LOGIN: URL_SYSTEM_V1 + "/auth/login",
   URL_REGISTER: URL_SYSTEM_V1 + "/auth/register",
